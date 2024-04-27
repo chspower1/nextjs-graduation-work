98e8dbfbeda2548a1ce6b3cd3dc1bdcdbd94af31
chs <chspower1@gmail.com>
2024-04-27 21:23:20 +0900

    feat : apply sebang gothic font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,13 @@ const config: Config = {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        primary: "#1383EB",
+        stroke: "#EFEFEF",
+        black: "#333333",
+        warning: "#E85454",
       },
     },
   },
