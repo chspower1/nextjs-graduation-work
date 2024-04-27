2924a5e644b0f1d121ca0ea58aeda59a9da9f93d
chs <chspower1@gmail.com>
2024-04-27 21:24:33 +0900

    feat : added login feature

--- app/log-in/action.ts
+++ app/log-in/action.ts
@@ -1,7 +1,65 @@
 "use server";
-export const loginAction = (prevState: any, formData: FormData) => {
-  const email = formData.get("email");
-  const password = formData.get("password");
-  console.log(prevState, email, password);
-  return { email, password };
+
+import db from "@/libs/db.util";
+import { LoginInput } from "@/typings/auth.type";
+import { z } from "zod";
+import bcrypt from "bcrypt";
+import { getSession, saveSession } from "@/libs/session.util";
+import { redirect } from "next/navigation";
+const loginSchema = z
+  .object({
+    [LoginInput.EMAIL]: z.string().trim(),
+    [LoginInput.PASSWORD]: z.string().trim(),
+  })
+  .superRefine(async ({ email, password }, ctx) => {
+    const user = await db.user.findUnique({
+      where: {
+        email,
+      },
+      select: {
+        id: true,
+        password: true,
+      },
+    });
+
+    // guard
+    if (!user) {
+      return ctx.addIssue({
+        code: "custom",
+        path: [LoginInput.EMAIL],
+        message: "존재하지 않는 아이디입니다.",
+      });
+    }
+    const isCorrectPassword = await bcrypt.compare(password, user.password);
+
+    if (!isCorrectPassword) {
+      return ctx.addIssue({
+        code: "custom",
+        path: [LoginInput.PASSWORD],
+        message: "비밀번호가 일치하지 않습니다.",
+      });
+    }
+
+    const session = await getSession();
+    saveSession(session, user.id);
+  });
+
+export const loginAction = async (prevState: any, formData: FormData) => {
+  const data = {
+    [LoginInput.EMAIL]: formData.get(LoginInput.EMAIL),
+    [LoginInput.PASSWORD]: formData.get(LoginInput.PASSWORD),
+  };
+  let errorMessage = null;
+  const result = await loginSchema.safeParseAsync(data);
+
+  if (!result.success) {
+    errorMessage = result.error.flatten();
+  } else {
+    redirect("/");
+  }
+  return {
+    email: data[LoginInput.EMAIL],
+    password: data[LoginInput.PASSWORD],
+    errorMessage,
+  };
 };
--- app/log-in/page.tsx
+++ app/log-in/page.tsx
@@ -2,17 +2,32 @@
 import { useFormState } from "react-dom";
 import { loginAction } from "./action";
 import SubmitButton from "@/components/submit-button";
+import { LoginInput } from "@/typings/auth.type";
+import Input from "@/components/Input";
 
 const LoginPage = () => {
   const [state, action] = useFormState(loginAction, null);
   return (
-    <div>
-      <form action={action}>
-        <label htmlFor="login-email">Email</label>
-        <input id="login-email" type="email" name="email" />
-        <label htmlFor="login-password">Password</label>
-        <input id="login-password" type="password" name="password" />
-        <SubmitButton>login</SubmitButton>
+    <div className="w-[288px] m-auto pt-[100px] flex flex-col items-center gap-[30px] text-black">
+      <h1 className="text-[32px] font-black ">로그인</h1>
+      <form className="w-full flex flex-col" action={action}>
+        <Input
+          id="create-account-email"
+          label="이메일"
+          name={LoginInput.EMAIL}
+          type="email"
+          errorMessage={state?.errorMessage?.fieldErrors[LoginInput.EMAIL]?.[0]}
+        />
+        {/* TODO: 비밀번호 마스킹하기 */}
+        <Input
+          id="create-account-password"
+          label="비밀번호"
+          name={LoginInput.PASSWORD}
+          type="password"
+          errorMessage={state?.errorMessage?.fieldErrors[LoginInput.PASSWORD]?.[0]}
+        />
+
+        <SubmitButton>로그인</SubmitButton>
       </form>
     </div>
   );
