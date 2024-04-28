75374c4152c6fd74b8690fee2878774d4fe2a103
조호성 <107151296+chspower1@users.noreply.github.com>
2024-04-28 15:15:32 +0000

    deploy

--- libs/session.util.ts
+++ libs/session.util.ts
@@ -4,7 +4,7 @@ import { cookies } from "next/headers";
 export const getSession = async () => {
   const session = await getIronSession<{ id?: number }>(cookies(), {
     cookieName: "iron-session",
-    password: process.env.SECRET_KEY!,
+    password: "3Whu9fU5NUBmZ9axTLPLzzvCm7JGwu2MnT66c1x0gU0",
   });
   return session;
 };
@@ -13,7 +13,7 @@ export const saveSession = async (
   session: IronSession<{
     id?: number;
   }>,
-  id: number
+  id: number,
 ) => {
   session.id = id;
   await session.save();
